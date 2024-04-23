fe15ef1aa44e665d6db67ebe8189147b93a5ecf0
Avinash P <aviputhukulangara@gmail.com>
2024-04-23 16:33:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import styled from 'styled-components';
+
+
 
 function App() {
+
+  const PropsBackground = styled.div(props => ({
+    background: props.$background,
+    height: '100vh',
+    width: '100%',
+    backgroundColor: '#e1e6e6'
+  }));
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <PropsBackground/>
+      </div>
     </div>
   );
 }
